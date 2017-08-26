7da12bfe5a10cde41e18210c08a0267a2222cbc5
wenshao <wenshao314106@163.com>
2017-08-26 11:41:04 +0800

    更新登录页面

--- src/routes/users.js
+++ src/routes/users.js
@@ -17,7 +17,7 @@ router.get('/login',async (ctx,next)=>{
 router.post('/login',async (ctx,next)=>{
     let params = ctx.request.body;
     if (params.userName=="admin456"){
-        ctx.session.id="admin123";
+        ctx.session.id="admin456";
         ctx.redirect('/');
     }else{
         //ctx.redirect('/users/login');
